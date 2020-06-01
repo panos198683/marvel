ace3e3b77ef03ec7f8e7cafcb559f7921e246a6e
Vasileios Pigadas <vassilis.pigadas@gmail.com>
2020-06-01 17:43:24 +0300

    minor changes

--- app/src/main/java/com/example/marvel/SignUpActivity.java
+++ app/src/main/java/com/example/marvel/SignUpActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
@@ -35,6 +36,14 @@ EditText firstname,lastname,nickname,emailsu,passwordsu;
         mAuth = FirebaseAuth.getInstance();
         mDatabaseHelper= new DatabaseData(this);
 
+        Button button = findViewById(R.id.signup);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signupclicked(v);
+            }
+        });
+
 
     }
     public void gotosignin(View view){
@@ -51,7 +60,12 @@ EditText firstname,lastname,nickname,emailsu,passwordsu;
         passwordsu=findViewById(R.id.passwordsu);
 
         if (firstname.length()!=0 && lastname.length()!=0 && nickname.length()!=0 && emailsu.length()!=0 && passwordsu.length()!=0){
+
+
             AddData(firstname.getText().toString(),lastname.getText().toString(),nickname.getText().toString(),emailsu.getText().toString(),passwordsu.getText().toString());
+
+
+
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
 //                    mAuth.createUserWithEmailAndPassword(emailsu.getText().toString(),
@@ -73,6 +87,9 @@ EditText firstname,lastname,nickname,emailsu,passwordsu;
 
     public void AddData(String firstname,String lastname,String nickname,String email,String password){
         boolean insertData = mDatabaseHelper.addData(firstname,lastname,nickname,email,password);
+        if(!insertData){
+            new IllegalArgumentException().printStackTrace();
+        }
     }
 
     private void userdetails(EditText firstname, EditText lastname, EditText nickname){
